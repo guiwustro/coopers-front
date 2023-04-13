df7f3118320dcef4f45009022acd89762e8bd7b2
Guilherme <gui.wustro@gmail.com>
2023-04-13 00:25:52 -0300

    feat(home): create home component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,43 @@
+import Banner from "../../components/Banner";
+import Header from "../../components/Header";
+import ModalLogin from "../../components/ModalLogin";
+import { TaskCard } from "../../components/TaskCard";
+import ToDoTitle from "../../components/ToDoTitle";
+import { useModalContext } from "../../contexts/ModalContext";
+import { useTaskContext } from "../../contexts/TaskContext";
+import { CardContainer, CardList } from "./styles";
+import Grafismo from "../../assets/grafismos-lateral-esquerda.svg";
+
+export const Home = () => {
+	const { isModalOpen } = useModalContext();
+	const { completedTasks, progressTasks } = useTaskContext();
+
+	return (
+		<>
+			<Header />
+			<Banner />
+			<ToDoTitle />
+			<CardContainer>
+				<img src={Grafismo} alt="grafismos" className="to-do__grafismo" />
+				<CardList>
+					<TaskCard
+						tasks={progressTasks}
+						borderColor="orange"
+						title="To-do"
+						subtitle="Take a breath. Start doing."
+						statusTask="progress"
+					/>
+					<TaskCard
+						tasks={completedTasks}
+						borderColor="green"
+						title="Done"
+						subtitle="You have done 5 tasks."
+						statusTask="done"
+					/>
+				</CardList>
+			</CardContainer>
+
+			{isModalOpen && <ModalLogin />}
+		</>
+	);
+};
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+	display: flex;
+	max-width: 1440px;
+`;
+
+export const CardContainer = styled.div`
+	padding-top: 40px;
+	padding-bottom: 68px;
+	position: relative;
+	.to-do__grafismo {
+		position: absolute;
+	}
+`;
+
+export const CardList = styled.div`
+	display: flex;
+	justify-content: center;
+	gap: 41px;
+`;
